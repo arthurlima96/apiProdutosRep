95b60e68fc76c0320c128a498d417a5a07ee66dc
arthurlima96 <arthurcantanhede@hotmail.com>
2018-07-02 10:53:55 -0300

    Primeiro Commit

---
+++ app/Http/Controllers/ProdutoController.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Produto;
+
+class ProdutoController extends Controller
+{
+
+    public function __construct()
+    {
+        header('Access-Control-Allow-Origin: *');
+    }
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $dados = $request->all();
+        $produto = Produto::create($dados);
+
+        if ($produto) {
+            return response()->json(['data'=>$produto,'status'=>true]);
+        }else{
+            return response()->json(['data'=>'Error ao criar produto','status'=>false]);
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
